1e93b916dc2d5e14c6cc2a6f3ce3865e480e33f3
extasy44 <seoheejun@gmail.com>
2020-06-05 23:43:29 +1000

    Unit testing for CommentBox, CommentList and intertaion testing for Fetching Comment

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+describe("should render components", () => {
+  let wrapped;
+
+  beforeEach(() => {
+    wrapped = shallow(<App />);
+  });
+
+  it("Shows a comment box", () => {
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+  });
+
+  it("Shows a comment list", () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
+  });
+});
